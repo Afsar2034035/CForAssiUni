33d13e40511cba4a485eff7b2453a9258b431183
Afsar2034035 <ak0090764@gmail.com>
2021-10-07 00:18:47 +0600

    Sheet 1 N No Solution Added

---
+++ Sheet1/N.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char c;
+    cin>>c;
+    if(c>=65 && c<=90)
+    {
+        c = c + 32;
+        cout<<c<<endl;
+    }
+    else
+    {
+        c = c - 32;
+        cout<<c<<endl;
+    }
+}
