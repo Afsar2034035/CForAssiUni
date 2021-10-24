105616110b2de556386e5ce128326ab6d5bdf4da
Afsar2034035 <ak0090764@gmail.com>
2021-10-24 20:47:42 +0600

    Sheet 4 A No Solution Added

---
+++ Sheet4/A.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string s1,s2;
+    cin>>s1>>s2;
+    int a,b;
+    a = s1.length();
+    b = s2.length();
+
+    cout<<a<<" "<<b<<endl;
+    cout<<s1<<" "<<s2<<endl;
+}
+
