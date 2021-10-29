41faffa6dc2003084b6e880898fd3d5af2addbf6
Afsar2034035 <ak0090764@gmail.com>
2021-10-29 18:54:05 +0600

    Test Added

---
+++ Test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int k,s,cnt = 0;
+    cin>>k>>s;
+    for(int x = 0; x <= k; x++)
+    {
+        for(int y = 0; y <= k; y++)
+        {
+            int z = s - x - y;
+            if(z <= k && z >= 0)
+                cnt++;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
