8131a684890fba87b4127dfaa8b0bef21ee71ca3
Afsar2034035 <ak0090764@gmail.com>
2021-10-31 00:31:35 +0600

    Contest 1 C No Solution Added

---
+++ Contest1/C.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char c;
+    cin>>c;
+    if(c=='z')
+        cout<<"a"<<endl;
+    else
+    {
+        c++;
+        cout<<c<<endl;
+    }
+}
