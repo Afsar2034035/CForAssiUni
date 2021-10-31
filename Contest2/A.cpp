f2745ef4b052adb9baf67cb1fd94262950dc4ce7
Afsar2034035 <ak0090764@gmail.com>
2021-11-01 00:11:27 +0600

    Contest 2 A No Solution Added

---
+++ Contest2/A.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long int a,b,c;
+    cin>>a>>b;
+    c=a-b;
+    if(c>=0)
+        cout<<c<<endl;
+    else
+        cout<<"0"<<endl;
+
+    return 0;
+}
