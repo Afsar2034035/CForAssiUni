3f1c9aef586ff7df684c812173460c279f70a655
Afsar2034035 <ak0090764@gmail.com>
2021-11-02 01:24:11 +0600

    Contest'22 1 A No Solution Added

---
+++ Contest'22 1/A.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long int x;
+    cin>>x;
+
+    if(x==1 || x%2==0)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+
+    return 0;
+}
